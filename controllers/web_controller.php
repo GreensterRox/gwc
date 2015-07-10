740b1a815389eef47ff64c15b6d1a5176894d15e
Dan Perry <perry.daniel@gmail.com>
2015-07-10 19:59:35 +0100

    First commit in the new order. Made controller path dynamic

--- controllers/web_controller.php
+++ controllers/web_controller.php
@@ -2,8 +2,7 @@
 set_exception_handler( 'gwc_exception_handler' );
 
 # Centralised Web controller - all sites will have $GWC available to them
-# TO DO Use __FILE__ to figure out where main class lives?? Or we could bundle the gwc library with each new site??
-$directory_path = '/data/green_software/green_framework/';
+$directory_path = str_replace('controllers','',__DIR__);
 include_once($directory_path.'classes/green_web_controller.php');
 try {
 	$GWC = new green_web_controller();
