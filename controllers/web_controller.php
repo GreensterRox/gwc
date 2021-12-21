40cf1cd0357a16262cc3309481e2f6c1936c1cf2
GreensterRox <greensterrox@voteworld.co.uk>
2021-12-21 17:24:59 +0000

    Added static exception handler web page and supressed stack trace

--- controllers/web_controller.php
+++ controllers/web_controller.php
@@ -9,12 +9,31 @@ try {
 	$GWC = new green_web_controller();
 	$GWC->handleRequest();
 } catch(Exception $ex){
-	//die('Cannot load website right now.  :-(');
-	die('need an elegant way of handling this - hard coded template or somming, etc ['.$ex.']');
+	gwc_exception_handler($ex);
 }
 
 function gwc_exception_handler($exception){
-	//die('Unhandled exception! Cannot load website right now ;-(');
-	die('Unhandled exception caught. Need an elegant way of handling this - hard coded template or somming, etc ['.$exception->getMessage().']');
+	$style = "body { background-color: #EFEFEF; color: #2E2F30; text-align: center; font-family: arial, sans-serif; }  div.dialog { width: 25em; margin: 4em auto 0 auto; border: 1px solid #CCC;  border-right-color: #999; border-left-color: #999; border-bottom-color: #BBB; border-top: #B00100 solid 4px; border-top-left-radius: 9px; border-top-right-radius: 9px; background-color: white; padding: 7px 4em 0 4em; }  h1 { font-size: 100%; color: #730E15; line-height: 1.5em; }  body > p { width: 33em; margin: 0 auto 1em; padding: 1em 0; background-color: #F7F7F7; border: 1px solid #CCC; border-right-color: #999; border-bottom-color: #999; border-bottom-left-radius: 4px; border-bottom-right-radius: 4px; border-top-color: #DADADA; color: #666; box-shadow:0 3px 8px rgba(50, 50, 50, 0.17); }";
+	$error = $exception->getMessage();
+	echo <<<EOL
+<!DOCTYPE html>
+<html>
+<head>
+<title>GWC - Critical Error</title>
+<style>
+$style
+</style>
+</head>
+
+<body>
+<div class="dialog">
+<h1>An unexpected error has occured.</h1>
+<p>Please try refreshing the page or come back later.</p>
+</div>
+<p>If you are an administrator of the site, please check the application logs for more information.<br/>The exception reported was [$error].</p>
+</body>
+</html>
+EOL;
+die();
 }
 ?>
